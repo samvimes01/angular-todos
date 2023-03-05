c3ecc39715b45654d2f3311936278fd0d73d78de
samvimes01 <Oleksandr.Korneiko@gmail.com>
2023-03-05 17:57:33 +0200

    Add more tests

--- src/features/todo/todos.service.spec.ts
+++ src/features/todo/todos.service.spec.ts
@@ -12,6 +12,7 @@ describe('TodosService', () => {
 
   beforeEach(() => {
     const spy = jasmine.createSpyObj('StorageService', ['get', 'set']);
+    spy.get.withArgs('tdl_todos').and.returnValue(stubValue);
 
     TestBed.configureTestingModule({
       // Provide both the service-to-test and its (spy) dependency
@@ -22,7 +23,6 @@ describe('TodosService', () => {
     valueServiceSpy = TestBed.inject(
       StorageService
     ) as jasmine.SpyObj<StorageService>;
-    valueServiceSpy.get.withArgs('tdl_todos').and.returnValue(stubValue);
   });
 
   it('should read storage on init', () => {
@@ -31,8 +31,56 @@ describe('TodosService', () => {
 
   it('should get list of items', (done: DoneFn) => {
     service.getList().subscribe((list) => {
-      expect(list).toEqual([]);
-      // expect(list).toEqual(stubValue);
+      expect(list).toEqual(stubValue);
+      done();
+    });
+  });
+
+  it('should get filtered list of items', (done: DoneFn) => {
+    service.search$.next('ed2');
+    service.getFilteredList().subscribe((list) => {
+      expect(list).toEqual([stubValue[1]]);
+      done();
+    });
+  });
+
+  it('should add todo item', (done: DoneFn) => {
+    service.addItem('mocked3', () => 789);
+    service.getList().subscribe((list) => {
+      expect(list).toEqual([
+        ...stubValue,
+        { id: 789, label: 'mocked3', isDone: false },
+      ]);
+      done();
+    });
+  });
+
+  it('should mark a todo item as done', (done: DoneFn) => {
+    service.toggleItem(123, true);
+    service.getList().subscribe((list) => {
+      expect(list).toEqual([{ ...stubValue[0], isDone: true }, stubValue[1]]);
+      done();
+    });
+  });
+
+  it('should update a todo item label', (done: DoneFn) => {
+    service.editItem(123, 'mocked1Edited');
+    service.getList().subscribe((list) => {
+      expect(list).toEqual([
+        { ...stubValue[0], label: 'mocked1Edited' },
+        stubValue[1],
+      ]);
+      done();
+    });
+  });
+
+  it('should save all items on list change', (done: DoneFn) => {
+    service.addItem('mocked3', () => 789);
+    service.getList().subscribe((list) => {
+      expect(valueServiceSpy.set).toHaveBeenCalledWith('tdl_todos', [
+        ...stubValue,
+        { id: 789, label: 'mocked3', isDone: false },
+      ]);
       done();
     });
   });
